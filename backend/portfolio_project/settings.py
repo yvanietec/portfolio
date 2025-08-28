b7b271c244150d9cf474eb294822ce6ef8b5cc36
yvanietec <yvanietec@gmail.com>
2025-08-28 08:53:02 +0530

    Refactor code structure for improved readability and maintainability

--- backend/portfolio_project/settings.py
+++ backend/portfolio_project/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static'
 STATICFILES_DIRS = [BASE_DIR / "static"]  # development static
 STATIC_ROOT = BASE_DIR / "staticfiles"    # for collectstatic
 
